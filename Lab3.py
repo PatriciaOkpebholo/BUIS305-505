f8f91357f62b61d18c19c332e4bd5854f2b1a5d3
PatriciaOkpebholo <144290844+PatriciaOkpebholo@users.noreply.github.com>
2023-10-16 19:56:46 -0400

    Lab 3

---
+++ Lab3.py
@@ -0,0 +1,63 @@
+import math
+
+number1=input('Enter number1')
+number2=input('Enter number2')
+print(number1,number2)
+sum= int(number1)+int(number2)
+print(sum)
+
+
+
+#number1=input('Enter number1')
+#number2=input('Enter number2')
+print(number1,number2)
+difference=int(number2)-int(number1)
+print(difference)
+
+print(number1,number2)
+multipy=int(number1)*int(number2)
+print(multipy)
+
+print(number1,number2)
+division=int(number1)/int(number2)
+print(division)
+
+
+#number1=input('Enter number1')
+#number2=input('Enter number2')
+number3=input('Enter number3')
+print(number1,number2,number3)
+sum= int(number1)+ int(number2)+int(number3)
+print(sum)
+
+
+print(number1,number2,number3)
+difference=int(number1)-int(number2)-int(number3)
+print(difference)
+
+print(number1,number2,number3)
+multipy=int(number1)*int(number2)*int(number3)
+print(multipy)
+
+print(number1,number2,number3)
+division=int(number1)/int(number2)/int(number3)
+print(division)
+
+
+
+
+print(math.cos(0.00))
+print(math.sqrt(9))
+print(math.sin(-1.23))
+print(math.acos(0))
+print(math.asin(1))
+print(math.erf(1.34))
+print(math.floor(22.6))
+print(math.fsum([1,2,3,4]))
+print(math.trunc(2.77))
+print(math.tan(90))
+
+
+
+
+
