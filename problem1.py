622d86df95971c0e1a66cd1573ddbc8bf9787d21
PatriciaOkpebholo <144290844+PatriciaOkpebholo@users.noreply.github.com>
2023-11-01 03:59:38 -0400

    Mid Term Exam (Problem1)

---
+++ problem1.py
@@ -0,0 +1,9 @@
+#### collect two inputs from the user to calculate the area of a rectangle;
+# you need to define at least two variables; collect the length and breadth;
+# calculate the area and print the area of the rectangle as output
+
+
+l = float(input('enter the length of a rectangle:'))
+b = float(input('enter the breadth of a rectangle:'))
+Area = l*b
+print('Area of a rectangle is:%.2f' % Area)
