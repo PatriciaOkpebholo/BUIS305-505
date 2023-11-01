aff0f86d25f710cc1f38eff8586df54d912857b6
PatriciaOkpebholo <144290844+PatriciaOkpebholo@users.noreply.github.com>
2023-11-01 13:20:13 -0400

    Mid-Term Exam (Problem4)

---
+++ problem4.py
@@ -0,0 +1,29 @@
+#### Construct your own example of a "for loop"
+# and write python code to demonstrate your understanding of a "For Loop.
+# { Hints:Refer to chapter 3 book or examples provided in the lab/class
+
+# for loop syntax (
+  # for var in iterable :
+    # statements
+
+#iteration in a list
+
+l = ["Today","is","cold"]
+for i in l:
+    print(i)
+
+# for loop with a range() function
+
+for i in range(5, 33, 2):
+    print(i)
+#for addition or summation
+
+sum=0.0
+number1 = 21
+number2 =22
+number3 =23
+number4 =24
+number5 =26
+for eachpass in range(5):
+    sum=sum+eachpass+number5
+    print(sum)
